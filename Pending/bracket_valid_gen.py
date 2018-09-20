6c329255e8361e8292945bae50d5940e4b22ed69
projectPythonator <ard902@mail.usask.ca>
2018-09-20 17:57:28 -0600

    Update bracket_valid_gen.py

--- Pending/bracket_valid_gen.py
+++ Pending/bracket_valid_gen.py
@@ -1,14 +1,17 @@
+
 def valid_pairs(n):
-  if n < 1:
-    return ''
-  pairs = set(['()'])
-  for i in range(1, n):
-    temp = set()
-    for p in pairs:
-      temp.add(p+'()')
-      temp.add('()'+p)
-      temp.add('('+p+')')
-    pairs = temp
-  return pairs
+    if n < 1:
+        return ''
+    #pairs of brackets that we will build up upon
+    pairs = set(['()'])
+    for i in range(1, n):
+        # fill set up with the next lvl n then set pairs to that level
+        temp = set()
+        for p in pairs:
+            temp.add(p+'()')
+            temp.add('()'+p)
+            temp.add('('+p+')')
+        pairs = temp
+    return pairs
 
 print(valid_pairs(20))

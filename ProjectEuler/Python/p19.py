7f787eefebfab6c028a469e44de3c7d84a5237b9
projectPythonator <ard902@mail.usask.ca>
2018-09-20 18:40:26 -0600

    Add files via upload

--- ProjectEuler/Python/p19.py
+++ ProjectEuler/Python/p19.py
@@ -1,1 +1,26 @@
-W…(¾¡ggÄ¤U
+#author agis daniels
+#How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000
+
+
+
+def main():
+	amtsuns = 0
+	mon_no_leap = [31,28,31,30,31,30,31,31,30,31,30,31]
+	mon_leap    = [31,29,31,30,31,30,31,31,30,31,30,31]
+	day=1
+	count=0
+	for year in range(1901,2001):
+		if year%4!=0:
+			for a in mon_no_leap:
+				day+=a
+				if day%7==6:
+					count+=1
+		else:
+			for a in mon_leap:
+				day+=a
+				if day%7==6:
+					count+=1
+	print(count)
+
+if __name__=='__main__':
+	main()

9643999b43ea858afde52a35bc29dd1d0d4b0a94
projectPythonator <ard902@mail.usask.ca>
2018-09-20 18:37:59 -0600

    Add files via upload

--- ProjectEuler/Python/p16.py
+++ ProjectEuler/Python/p16.py
@@ -1,12 +1,1 @@
-#author agis daniels
-#What is the sum of the digits of the number 2^1000?
-
-def main():
-	x=2**1000
-	print("the sum of digits of 2^1000 is below in three diff ways")
-	print(sum([int(i) for i in str(x)]))
-	print(sum(map(int, str(x))))
-	print(sum(map(int, [x for x in str(x)])))
-
-if __name__=='__main__':
-	main()
+Њ@йт"Чў4Ъ

ca69c35c56d3a182a6f2632a699a42c490176def
projectPythonator <ard902@mail.usask.ca>
2018-09-21 07:20:17 -0600

    Update p4.py

--- ProjectEuler/Python/p4.py
+++ ProjectEuler/Python/p4.py
@@ -1,1 +1,29 @@
 
+
+
+def is_palindrome(num):
+    return (str(num) == str(num)[::-1])
+
+
+def sol2(limit):
+    largest = 0
+    a = 999
+    lim = 100
+    while lim <= a:
+        b = 999
+        while a <= b:
+            ab = a*b
+            if ab < largest:
+                break
+            if is_palindrome(ab):
+                largest = ab
+            b -= 1
+        a -= 1
+    print('largest palindrone of three digit multile below {} is {}'.format(limit, largest))
+ 
+
+def main():
+    sol2(1000)
+
+
+main()

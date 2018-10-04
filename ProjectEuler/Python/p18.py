f3d00c4dd643d593ebb4036c0eb88b0a59aee71e
projectPythonator <ard902@mail.usask.ca>
2018-10-04 15:49:37 -0600

    Update p18.py

--- ProjectEuler/Python/p18.py
+++ ProjectEuler/Python/p18.py
@@ -44,6 +44,13 @@ def my_first_sol(graph, lvls):
 def sol2(graph, lvls):
   print('biggest sum path of the graph is {}'.format(my_first_sol(graph, lvls)))
 
+#much cleaner version of sol2 not mine but i like this clean short one that i seen 
+def sol3(name_of_file):
+  nodes = [[int(num) for num in line.split(' ')] for line in open(name_of_file).readlines()]
+  for i in range(len(nodes) - 2, -1, -1):
+    for j in range(i+1): nodes[i][j] += max(nodes[i+1][j], nodes[i+1][j+1])
+  print(nodes[0][0])
+
 def main():
   graph = [75,95,64,17,47,82,18,35,87,10,20,4,82,47,65,19,1,23,75,3,34,88,2,77,73,7,63,67,99,65,4,28,6,16,70,92,41,41,26,56,83,40,80,70,33,41,48,72,33,47,32,37,16,94,29,53,71,44,65,25,43,91,52,97,51,14,70,11,33,28,77,73,17,78,39,68,17,57,91,71,52,38,17,14,91,43,58,50,27,29,48,63,66,4,68,89,53,67,30,73,16,69,87,40,31,4,62,98,27,23,9,70,98,73,93,38,53,60,4,23,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   sol1(graph)

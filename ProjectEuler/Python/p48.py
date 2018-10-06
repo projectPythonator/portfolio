a7c97a9baee4936c020e593bc84b571fd8ecc219
projectPythonator <ard902@mail.usask.ca>
2018-10-06 16:53:35 -0600

    Update p48.py

--- ProjectEuler/Python/p48.py
+++ ProjectEuler/Python/p48.py
@@ -1,1 +1,16 @@
-ﬂËNG¥ Øπ
+import time
+from math import sqrt
+
+def helper(lim):
+  return str(sum([i**i for i in range(1, lim+1)]))[-10:]
+
+def sol1(lim):
+  print('lsat 10 digits of sum of i^i up to {} is {}'.format(lim, helper(lim)))
+
+def main():
+  a = time.clock()
+  sol1(1000)
+  b = time.clock()
+  print('time taken is {:f}'.format(b-a))
+
+main()

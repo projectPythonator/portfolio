b1c0efdd927a6756ea51d3b3af0d2aaf348741dc
projectPythonator <ard902@mail.usask.ca>
2018-10-07 13:38:18 -0600

    Update p52.py

--- ProjectEuler/Python/p52.py
+++ ProjectEuler/Python/p52.py
@@ -1,1 +1,23 @@
-P	¥ÿhiå∑â
+import time
+
+def check_num(n):
+  base = sorted(str(n))
+  for i in range(2, 7):
+    if base != sorted(str(n*i)):
+      return False
+  return True
+
+def sol1(lim):
+  for i in range(10, lim):
+    if check_num(i):
+      print(i)
+      return
+
+def main():
+  lim = 1000000
+  a = time.clock()
+  sol1(lim)
+  b = time.clock()
+  print('time taken is {:f}'.format(b-a))
+main()
+  

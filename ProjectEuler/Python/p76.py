15cbe03b503e1516ffaf60b321b2044a6740b364
projectPythonator <ard902@mail.usask.ca>
2018-10-20 08:54:18 -0600

    Update p76.py

--- ProjectEuler/Python/p76.py
+++ ProjectEuler/Python/p76.py
@@ -1,1 +1,23 @@
-Qü°¡×Ú6Q/¶
+from math import sqrt
+import time
+
+
+def sol1(amt):
+  way = [0]*(amt+1)
+  way[0] =1
+  for i in range(1, amt):
+    for j in range(i, amt+1):
+      way[j] += way[j-i]
+  print('The ways to sum up to {} is {}'.format(amt, way[amt]))
+
+  
+def main():
+  lim = 100
+  a = time.clock()
+  sol1(lim)
+  b = time.clock()
+  print('time taken is {:f}'.format(b-a))
+
+
+
+main()

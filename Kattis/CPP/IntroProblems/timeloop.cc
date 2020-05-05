c8a62d6f97aba42102083cc40f87f05a54fca88e
projectPythonator <ard902@mail.usask.ca>
2020-05-05 11:30:38 -0600

    Create timeloop.cc

---
+++ Kattis/CPP/IntroProblems/timeloop.cc
@@ -0,0 +1,23 @@
+/*
+kattis timeloop problem
+Author: Agis Daniels
+Solve print a count up from 1
+NOTE 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //declare n and read it in
+    int n=0;
+    cin>>n;
+    
+    // follow instructions as asked
+    for(int i=1; i<=n; ++i){
+        cout<<i<<" Abracadabra"<<endl;
+    }
+    return 0;
+}

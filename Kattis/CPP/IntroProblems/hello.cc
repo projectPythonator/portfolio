5583d8d6e218d57cff43ae8cebd42a713c4592ad
projectPythonator <ard902@mail.usask.ca>
2020-05-04 18:12:40 -0600

    Create hello.cc

---
+++ Kattis/CPP/IntroProblems/hello.cc
@@ -0,0 +1,14 @@
+/*
+kattis hello problem
+Author: Agis Daniels
+Solve the classic hello world 
+NOTE 
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+    cout<<"Hello World!\n";
+    return 0;
+}

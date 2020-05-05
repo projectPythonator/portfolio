70a43b9a102b9ca08fecc3377d500262ff0779a9
projectPythonator <ard902@mail.usask.ca>
2020-05-04 18:10:42 -0600

    classic hello world prog

---
+++ Kattis/Python/IntroProblems/hello.py
@@ -0,0 +1,11 @@
+""" 
+kattis hello problem
+Author: Agis Daniels
+Solve the classic hello world 
+NOTE 
+"""
+from sys import stdin as rf
+def main():
+    print("Hello World!")
+if __name__=='__main__':
+    main()

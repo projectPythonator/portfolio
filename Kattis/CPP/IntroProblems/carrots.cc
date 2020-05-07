42ef5672dc886f3b153cf379019f8359af0bc8b2
projectPythonator <ard902@mail.usask.ca>
2020-05-06 20:14:21 -0600

    Create carrots.cc

---
+++ Kattis/CPP/IntroProblems/carrots.cc
@@ -0,0 +1,17 @@
+/*
+kattis carrots problem
+Author: Agis Daniels
+Solve simple solution read the first line of input print out ignore the rest
+NOTE 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,p;
+    cin>>n>>p;
+    cout<<p<<endl;
+    return 0;
+}

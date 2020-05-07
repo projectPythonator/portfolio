0dd10c336bbda795618ffc2c53005b0f13e1cf94
projectPythonator <ard902@mail.usask.ca>
2020-05-06 19:53:12 -0600

    Create carrots.py

---
+++ Kattis/Python/IntroProblems/carrots.py
@@ -0,0 +1,13 @@
+""" 
+kattis hello problem
+Author: Agis Daniels
+Solve simple solution read the first line of input print out ignore the rest
+NOTE 
+"""
+
+from sys import stdin as rf
+def main():
+    n,p=map(int, rf.readline().split())
+    print(p)
+if __name__=='__main__':
+    main()

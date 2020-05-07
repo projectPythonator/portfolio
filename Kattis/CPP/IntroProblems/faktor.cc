75de2807f677d085dba5bf159f2a9363087603fc
projectPythonator <ard902@mail.usask.ca>
2020-05-06 20:29:21 -0600

    Create faktor.cc

---
+++ Kattis/CPP/IntroProblems/faktor.cc
@@ -0,0 +1,17 @@
+/*
+kattis faktor problem
+Author: Agis Daniels
+Solve simple algebra solve for x given a and i type of deal
+NOTE 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a,i;
+    cin>>a>>i;
+    cout<<a*(i-1)+1<<endl;
+    return 0;
+}
